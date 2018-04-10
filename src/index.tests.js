Polyconseil/bluepicker

commit fd7405cce8dc0a09628c1308432d23461b10b94c

allow manual reset of the date input
When the user manually clears the field, the state of the datepicker
should be reset.

src/index.tests.js
@@ -67,7 +67,7 @@ describe('Bluepicker mode days', () => {
     inputElt.value = date
     dispatchEvent(inputElt, 'change')
   })
-  it('should allow the selection of a day', () => {
+  it('should allow the selection of a day and its manual reset', (done) => {
     clickInputAndAssertDropdownIsVisible()
     const allDatesElts = Array.from(document.querySelectorAll(`.${styles.dow}`))
     const firstDateElt = allDatesElts.filter((el) => el.textContent === '15')[0]
@@ -76,6 +76,19 @@ describe('Bluepicker mode days', () => {
     const yearAndMonth = moment().format('YYYY-MM')
     const offset = getTzOffset(moment(`${yearAndMonth}-15`))
     expect(inputElt.value).toEqual(`${yearAndMonth}-15T00:00:00${offset}`)
+
+    // try to reset it and check that the event is sent upstream
+    const root = document.querySelector('#root')
+    let listener = null
+    listener = (e) => {
+      expect(e.detail.value).toBe(null)
+      root.removeEventListener('bluepicker:update', listener)
+      done()
+    }
+    root.addEventListener('bluepicker:update', listener)
+    inputElt.value = ''
+    dispatchEvent(inputElt, 'change')
+    expect(inputElt.value).toEqual('')
   })
 })
 

src/index.js
@@ -52,6 +52,7 @@ function enumerateDaysOfCalendar (currentDay, format = 'D') {
 
 
 function tableForDay (day, nowButtonText) {
+  day = day || moment()
   return tableTemplate({
     styles: styles,
     thead: dayHeaderTemplate({
@@ -156,21 +157,9 @@ export function init (
   if (initValue) {
     selectedDay = moment(initValue)
     inputField.value = selectedDay.format(format)
-  } else {
-    const today = moment()
-    selectedDay = moment({
-      year: today.year(),
-      month: today.month(),
-      date: 1,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0,
-    })
   }
 
   function dispatchUpdateEvent () {
-    if (!selectedDay) return // do not bother if no value yet
     const data = {
       id: id,
       format: format,
@@ -210,6 +199,7 @@ export function init (
   }
 
   function nextInputValue () {
+    if (!selectedDay) return ''
     if (utcMode) {
       return selectedDay.format(format)
     } else {
@@ -256,6 +246,7 @@ export function init (
           mainTable.innerHTML = tableForDay(selectedDay, nowButtonText)
         } else if (t.classList.contains(styles.dow)) {
           const clickedDay = moment(utils.getDataValue(t))
+          selectedDay = selectedDay || moment()
           selectedDay.set({
             'year': clickedDay.year(),
             'month': clickedDay.month(),
@@ -311,12 +302,15 @@ export function init (
 
   inputField.addEventListener('change', function (e) {
     let newDay = null
-    if (utcMode) {
-      newDay = moment.utc(inputField.value)
-    } else {
-      newDay = moment(inputField.value)
+    if (inputField.value) {
+      if (utcMode) {
+        newDay = moment.utc(inputField.value)
+      } else {
+        newDay = moment(inputField.value)
+      }
     }
-    if (newDay.isValid()) {
+    if (newDay == null || newDay.isValid()) {
+      // newDay = null => the user wants to reset the field
       selectedDay = newDay
       updateValue()
       hideAndResetTable()

commit bff1cf618968e73da930fc08cd0317c14c931469

add more tests
fd7405cce8dc0a09628c1308432d23461b10b94c broke previous, next and now
buttons.
Add tests to avoid future regressions.

src/styles.css
@@ -56,5 +56,6 @@
 :local(.moh) {}  // minute of hour
 :local(.hod) {}  // hour of day
 :local(.dow) {}  // day of week
+:local(.tableHeader) {}
 :local(.left) {}
 :local(.right) {}

src/table-header-day.dot
@@ -1,6 +1,6 @@
 <tr>
   <th class="{{=it.styles.left}} {{=it.styles.fakelink}}">&larr;</th>
-  <th colspan="5">{{=it.monthYear}}</th>
+  <th class="{{=it.styles.tableHeader}}" colspan="5">{{=it.monthYear}}</th>
   <th class="{{=it.styles.right}} {{=it.styles.fakelink}}">&rarr;</th>
 </tr>
 <tr>

src/table-header-hour.dot
@@ -1,3 +1,3 @@
 <tr>
-  <th colspan="7">{{=it.currentDay}}</th>
+  <th class="{{=it.styles.tableHeader}}" colspan="7">{{=it.currentDay}}</th>
 </tr>

src/table-header-minute.dot
@@ -1,3 +1,3 @@
 <tr>
-  <th colspan="7">{{=it.currentTime}}</th>
+  <th class="{{=it.styles.tableHeader}}" colspan="7">{{=it.currentTime}}</th>
 </tr>

src/index.tests.js
@@ -90,6 +90,59 @@ describe('Bluepicker mode days', () => {
     dispatchEvent(inputElt, 'change')
     expect(inputElt.value).toEqual('')
   })
+  it('should allow changing the month during the selection process', () => {
+    clickInputAndAssertDropdownIsVisible()
+    const headerFormat = 'YYYY-MMM'
+    expect(document.querySelector(`.${styles.tableHeader}`).textContent).toBe(moment().format(headerFormat))
+
+    function click(leftOrRight) {
+      // the table is rebuilt at every click => we need to refetch the buttons after each click.
+      let button = null
+      if (leftOrRight === 'left') {
+        button = document.querySelector(`.${styles.left}`)
+      } else if (leftOrRight === 'right') {
+        button = document.querySelector(`.${styles.right}`)
+      } else throw "click: Invalid value expected left or right."
+      button.click()
+    }
+
+    click('left')
+    click('left')
+    expect(document.querySelector(`.${styles.tableHeader}`).textContent).toBe(moment().subtract(2, 'month').format(headerFormat))
+
+    click('right')
+    click('right')
+    click('right')
+    click('right')
+    click('right')
+    expect(document.querySelector(`.${styles.tableHeader}`).textContent).toBe(moment().add(3, 'month').format(headerFormat))
+  })
+  it('should allow setting the date to now', async (done) => {
+    clickInputAndAssertDropdownIsVisible()
+    const nowButton = document.querySelector(`.${styles.nowButton}`)
+    const root = document.querySelector('#root')
+    const inputElt = document.querySelector('#root > input')
+
+    let resolveEventPropagation = null
+    const resolveEventPropagationPromise = new Promise((resolve) => {
+      resolveEventPropagation = resolve
+    })
+
+    const listener = (e) => {
+      // check that the event is well dispatched and that the input element is updated
+      expect(e.detail.value.diff(moment(), 'seconds')).toBeLessThan(61)  // seconds are set to 0 when clicking now
+      root.removeEventListener('bluepicker:update', listener)
+      resolveEventPropagation()
+    }
+    root.addEventListener('bluepicker:update', listener)
+
+    nowButton.click()
+
+    const yearAndMonthAndDay = moment().format('YYYY-MM-DD')
+    expect(inputElt.value.slice(0, 10)).toEqual(yearAndMonthAndDay)
+    await resolveEventPropagationPromise
+    done()
+  })
 })
 
 describe('Bluepicker mode hours', () => {